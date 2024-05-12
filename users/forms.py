948860b48b266b0312a4de9af0f495f1341c4937
zablon <zablonoigo51@gmail.com>
2024-05-12 18:57:45 +0300

    Customer user  creation form

---
+++ users/forms.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm
+from .models import CustomUser
+
+class CustomuserCreationForm(UserCreationForm):
+    class Meta:
+        model=CustomUser
+        fields=("email",)
