b5746ca06d8d154e49cfe650fd296a53c289cd3d
zablon <zablonoigo51@gmail.com>
2024-05-13 07:13:03 +0300

    Adding userchange form on admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .forms import CustomerUserChangeForm,CustomuserCreationForm
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
+class CutomUserAdmin(UserAdmin):
+    add_form=CustomuserCreationForm
+    form=CustomerUserChangeForm
+    model=CustomUser
+    list_display=["email","is_staff","is_active"]
+    list_filter=["email","is_staff","is_active"]
+    fieldsets=(
+        (None,{"fields":("email","password")}),
+        ("Permissions",{"fields":("is_staff","is_active","groups","user_permissions")}),
 
-# Register your models here.
+    )
+    
