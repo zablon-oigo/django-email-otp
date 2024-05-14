653b411d36e17299b0b46bb68b761bd9f64f06e2
zablon <zablonoigo51@gmail.com>
2024-05-14 10:08:30 +0300

    Changed create user method

--- users/managers.py
+++ users/managers.py
@@ -18,4 +18,4 @@ class CustomUserManager(BaseUserManager):
             raise ValueError("Superuser must have is_staff=True")
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True")
-        return self.create_superuser(email,password, **extra_fields)
+        return self.create_user(email,password, **extra_fields)
