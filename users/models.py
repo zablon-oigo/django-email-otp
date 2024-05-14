ca2217c2cad4c644594c0be8930c4f701514ddeb
zablon <zablonoigo51@gmail.com>
2024-05-14 10:20:11 +0300

    Token model

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin
 from .managers import CustomUserManager
+import secrets
 class CustomUser(AbstractBaseUser,PermissionsMixin):
     email=models.EmailField(verbose_name="Email address", unique=True)
     is_staff=models.BooleanField(default=False)
@@ -15,3 +16,9 @@ class CustomUser(AbstractBaseUser,PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+class OTPToken(models.Model):
+    user=models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='otps')
+    otp_code=models.CharField(max_length=6,default=secrets.token_hex(3))
+    created=models.DateTimeField(auto_now_add=True)
+    expire=models.DateTimeField(blank=True, null=True)
