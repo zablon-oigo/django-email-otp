c31598d022899b3217349c1d72c482013035edfa
zablon <zablonoigo51@gmail.com>
2024-05-16 21:50:59 +0300

    adding homepage view function

--- users/views.py
+++ users/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    context={
+        "title":"Home Page"
+    }
+    return render(request, "index.html", context)
