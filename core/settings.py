dca75c472e0b87302715daa982007934e30d9059
zablon <zablonoigo51@gmail.com>
2024-05-16 12:54:49 +0300

    adding email configurations

--- core/settings.py
+++ core/settings.py
@@ -123,3 +123,6 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL="users.CustomUser"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
