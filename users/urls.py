cf92f66999458698d3b3915335d48df221b43a5b
zablon <zablonoigo51@gmail.com>
2024-05-23 06:51:54 +0300

    Adding verify email path

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,6 @@ from .views import verify_email,login_user,register_user,logout_user
 urlpatterns=[
     path("login/", login_user, name="login"),
     path("register/", register_user, name="register"),
-    path("logout/", logout_user, name="logout")
+    path("logout/", logout_user, name="logout"),
+    path("verify-email/<slug:email>",verify_email, name="verify-email"),
 ]
