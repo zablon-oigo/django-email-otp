2e2a552c72b64a421bc13d3b502c6ca98167f99f
zablon <zablonoigo51@gmail.com>
2024-05-26 20:35:41 +0300

    Adding default email

--- users/signals.py
+++ users/signals.py
@@ -1,34 +1,30 @@
-from django.conf import settings
 from django.db.models.signals import post_save
+from django.conf import settings
 from django.dispatch import receiver
+from .models import OtpToken
 from django.core.mail import send_mail
 from django.utils import timezone
-from .models import OTPToken
-from django.contrib.auth import get_user_model
 
-User = get_user_model()
-
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=settings.AUTH_USER_MODEL) 
 def create_token(sender, instance, created, **kwargs):
     if created:
         if not instance.is_superuser:
-            OTPToken.objects.create(user=instance, expire=timezone.now() + timezone.timedelta(minutes=5))
+            OtpToken.objects.create(user=instance, otp_expires_at=timezone.now() + timezone.timedelta(minutes=5))
             instance.is_active = False
             instance.save()
-
-            otp = OTPToken.objects.filter(user=instance).last()
-            subject = "Email Verification"
-            message = f"""
-                        Hello {instance.email}, here is your OTP {otp.otp_code}
-                        It expires in 5 minutes. Use the URL below to redirect back to the website:
-                        http://127.0.0.1:8000/verify-email/{instance.email}
+        otp = OtpToken.objects.filter(user=instance).last()
+        subject = "Email Verification"
+        message = f"""
+                        Hi {instance.username}, here is your OTP {otp.otp_code}. 
+                        It expires in 5 minutes. Use the URL below to verify your email:
+                        http://127.0.0.1:8000/verify-email/{instance.username}
                     """
-            sender = settings.DEFAULT_FROM_EMAIL
-            receiver = [instance.email, ]
-            send_mail(
-                subject,
-                message,
-                sender,
-                receiver,
-                fail_silently=False,
-            )
+        sender = settings.DEFAULT_FROM_EMAIL
+        receiver = [instance.email]
+        send_mail(
+            subject,
+            message,
+            sender,
+            receiver,
+            fail_silently=False,
+        )
