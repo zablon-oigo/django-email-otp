4165ee7d30646c083c5984b9f54e82d48c91a346
zablon <zablonoigo51@gmail.com>
2024-05-26 20:34:27 +0300

    Token model

--- users/models.py
+++ users/models.py
@@ -1,27 +1,21 @@
 from django.db import models
-from django.utils import timezone
-from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin
-from .managers import CustomUserManager
+from django.contrib.auth.models import AbstractUser
 import secrets
-class CustomUser(AbstractBaseUser,PermissionsMixin):
-    email=models.EmailField(verbose_name="Email address", unique=True)
-    is_staff=models.BooleanField(default=False)
-    is_active=models.BooleanField(default=True)
-    date_joined=models.DateTimeField(default=timezone.now)
 
-    USERNAME_FIELD="email"
-    REQUIRED_FIELDS=[]
-
-    objects=CustomUserManager()
-
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+    
     def __str__(self):
         return self.email
-
-class OTPToken(models.Model):
-    user=models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='otps')
-    otp_code=models.CharField(max_length=6,default=secrets.token_hex(3))
-    created=models.DateTimeField(auto_now_add=True)
-    expire=models.DateTimeField(blank=True, null=True)
-
+    
+class OtpToken(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="otps")
+    otp_code = models.CharField(max_length=6, default=secrets.token_hex(3))
+    otp_created_at = models.DateTimeField(auto_now_add=True)
+    otp_expires_at = models.DateTimeField(blank=True, null=True)
+    
     def __str__(self):
-        return self.user.email
+        return self.user.username
